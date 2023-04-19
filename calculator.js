5c6da001a4484a09c0ff7248bce1220bd1dc281a
KrolPolski <ryanboudwin@outlook.com>
2023-04-19 09:40:13 +0300

    pressing equals sign additional times now repeats operations

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 let firstNumber = null;
 let secondNumber = null;
 let operator;
+let lastOperator;
+let lastSecondNumber;
 let strNumber = "";
 const results = document.querySelector(".results");
 function add(firstNumber,secondNumber) 
@@ -30,6 +32,7 @@ function operate(firstNumber, operator, secondNumber)
     }
 
     else if (operator === "/") return divide(firstNumber, secondNumber);
+
     else console.error("Invalid operator detected");
 }
 function clear()
@@ -51,6 +54,11 @@ ops.forEach(op => op.addEventListener('click', operatorPress));
 // add event listener for clear button
 const clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', clear);
+
+// add event listener for equals button
+const equalsBtn = document.querySelector('#keyEquals');
+equalsBtn.addEventListener('click', equalsPress);
+
 
 function keyPress()
 {
@@ -83,6 +91,43 @@ function operatorPress()
         results.textContent = operate(firstNumber, operator, secondNumber);
         }
     //else console.error("Both numbers already assigned, something went wrong");
+}
+function equalsPress() 
+{
+    if (firstNumber === null) {
+        firstNumber = parseInt(strNumber);
+        strNumber = "";                
+    }
+    else if (secondNumber === null && operator != null) {
+        secondNumber = parseInt(strNumber);
+        let answer = operate(firstNumber, operator, secondNumber);
+        results.textContent = answer;        
+        strNumber = "";
+        firstNumber = answer;
+        lastOperator = operator;
+        lastSecondNumber = secondNumber;
+        secondNumber = null;
+        operator = null;
+    }
+    else if (secondNumber === null && operator === null)
+    {
+        console.log("No operator defined so repeating last operation against existing total")
+        let answer = operate(firstNumber, lastOperator, lastSecondNumber);
+        results.textContent = answer;        
+        strNumber = "";
+        firstNumber = answer;
+        secondNumber = null;
+    }
+
+    else {
+        console.log("both numbers are defined already so something went wrong")    
+       /* let answer = operate(firstNumber, lastOperator, lastSecondNumber);
+        results.textContent = answer;        
+        strNumber = "";
+        firstNumber = answer;
+        secondNumber = null;
+        */
+        }
 }
 console.log(operate(5,"+",3));
 console.log(operate(2, "*", 3));
